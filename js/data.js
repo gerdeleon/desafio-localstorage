5b39d8223aeb2139f07afaf658fa295fce5fdc03
German De León <141041025+gerdeleon@users.noreply.github.com>
2023-08-21 11:12:40 -0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dataElement = document.getElementById("data");
+    const storedValue = localStorage.getItem("userInput");
+  
+    if (storedValue) {
+      dataElement.textContent = storedValue;
+    } else {
+      dataElement.textContent = "No hay datos almacenados.";
+    }
+  });
